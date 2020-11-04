b454da33688dbc10a0f250ab527c8ad4f2bb6693
Marcos Gaspar <Marga5@users.noreply.github.com>
2020-11-04 16:13:21 +0000

    Criação das structs
    Primeira versão das estruturas de dados

--- cliente.c
+++ cliente.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "header.h"
+
+void main() {
+	printf("Insira o Username: ");
+	scanf("%s", Jogador.nome);
+
+	printf("Nome: ", Jogador.nome);
+}
---
+++ header.h
@@ -0,0 +1,19 @@
+
+struct Jogador;
+struct Jogo;
+struct Campeonato;
+
+typedef Campeonatos{
+	int id;
+}Campeonato;
+
+typedef Jogos{
+	int id;
+	
+}Jogo;
+
+typedef Jogadores{
+	int id;
+	int username;
+	int pontuacao;
+}Jogador;
